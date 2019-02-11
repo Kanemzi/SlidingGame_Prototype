8d50091813d86b1baaff34b5b6c5c5d458e202e4
Simon Rozec <simon.rozec1@etu.univ-nantes.fr>
2019-02-11 17:26:56 +0100

    Ajout des blocs-pentes

--- scripts/Level1.gd
+++ scripts/Level1.gd
@@ -9,6 +9,7 @@ var grid = []
 var player : Node2D
 var playerPos : Vector2
 var moveComplete = false
+var slopeMove : Vector2
 
 func _ready():
 	
@@ -25,8 +26,7 @@ func _ready():
 	pass
 	
 func _on_player_move(direction : Vector2) :
-	$moveTimeout.start()
-	
+
 	moveComplete = false
 	
 	while not moveComplete :
@@ -45,12 +45,18 @@ func _on_player_move(direction : Vector2) :
 		player.tween.start()
 		
 		yield(player.tween, "tween_completed")
-		
+		print('ende')
 		var currentCell = grid[playerPos.y][playerPos.x]
 		
-		if currentCell == null :
+		if currentCell == null || (not currentCell.is_in_group('blocks') && not currentCell.is_in_group('slopeblocks') ):
+			print('block')
 			moveComplete = true
-	
+			
+		elif currentCell.is_in_group('slopeblocks'):
+			direction = slopeMove
+			print('fuuush')
+			pass 
+			
 	emit_signal("move_end")
 	pass
 
@@ -62,13 +68,53 @@ func move_until_collision(direction : Vector2) :
 	
 	var stop = false
 	
-	while grid[checked.y][checked.x] == null || not stop :
+	while not stop :
 		if (checked.x < 0 || checked.x >= grid[0].size() || checked.y < 0 || checked.y >= grid.size()) :
 			return checked + direction * 100
 		
 		var cell = grid[checked.y][checked.x]
-		if cell.is_in_group('blocks') :
-			stop = true
+		if grid[checked.y][checked.x] != null :
+			if cell.is_in_group('blocks') :
+				stop = true
+				break
+			elif cell.is_in_group('slopeblocks') && checked != playerPos:
+				slopeMove = Vector2(-1, 0)
+				var id = cell.id
+				stop = true
+				if id == 4 :
+					if direction.y == -1 :
+						slopeMove = Vector2(1, 0)
+					elif direction.x == -1 :
+						slopeMove = Vector2(0, 1)
+					else :
+						break
+					pass
+				elif id == 5 :
+					if direction.y == -1 :
+						slopeMove = Vector2(-1, 0)
+					elif direction.x == 1 :
+						slopeMove = Vector2(0, 1)
+					else :
+						break
+					pass
+				elif id == 6 :
+					if direction.y == 1 :
+						slopeMove = Vector2(-1, 0)
+					elif direction.x == 1 :
+						slopeMove = Vector2(0, -1)
+					else :
+						break
+					pass
+				else :
+					if direction.y == 1 :
+						slopeMove = Vector2(1, 0)
+					elif direction.x == -1 :
+						slopeMove = Vector2(0, -1)
+					else :
+						break
+					pass
+				
+				pass
 		
 		checked += direction
 		
